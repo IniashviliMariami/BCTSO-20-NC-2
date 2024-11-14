3ee1a7fded6e648c8de295aa59665e144b137f06
mariaminiashvili <123764110+mariaminiashvili@users.noreply.github.com>
2024-11-14 19:44:26 +0400

    Add project files.

---
+++ MiniBank.Repository/AccountJsonRepository.cs
@@ -0,0 +1,72 @@
+﻿using MiniBank.Models;
+using System.Linq.Expressions;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MiniBank.Repository
+{
+    public class AccountJsonRepository
+    {
+        private readonly string _filePath;
+        private List<Account> _accounts;
+
+        public AccountJsonRepository(string filePath)
+        {
+            _filePath = filePath;
+            _accounts = LoadData();
+        }
+
+        public List<Account> GetAccounts() => _accounts;
+
+        public List<Account> GetAccountsOfCustomer(int customerId) => _accounts.Where(x => x.CustomerId == customerId).ToList();
+
+        public Account GetAccount(int id) => _accounts.FirstOrDefault(x => x.Id == id);
+
+        public void Create(Account account)
+        {
+            _accounts.Add(account);
+            SaveData();
+        }
+
+        public void Update(Account account)
+        {
+            var changeId = _accounts.FirstOrDefault(a => a.Id == account.Id);
+            if (changeId.Id >= 0)
+            {
+                _accounts[changeId] = account;
+                SaveData();
+            }
+        }
+
+        public void Delete(int id)
+        {
+           var deleteId=_accounts.FirstOrDefault(a=>a.Id== id);
+            if(deleteId.Id != null)
+            {
+                _accounts.Remove(deleteId);
+                SaveData();
+            }
+
+        }
+
+        private void SaveData()
+        {
+
+            string jsonData=JsonSerializer.Serialize(_accounts, new JsonSerializerOptions());
+            File.WriteAllText(_filePath,jsonData);
+           
+        }
+
+        private List<Account> LoadData()
+        {
+            if (!File.Exists(_filePath))
+            {
+               return new List<Account>();
+            }
+           string text = File.ReadAllText(_filePath);
+           var dataList = JsonSerializer.Deserialize<List<Account>>(text);
+            return dataList;
+        }
+    }
+}
---
+++ MiniBank.Tests/Account_Json_Repository_Should.cs
@@ -0,0 +1,66 @@
+﻿using MiniBank.Repository;
+using MiniBank.Models;
+using System.Xml.Linq;
+
+namespace MiniBank.Tests
+{
+    public class Account_Json_Repository_Should
+    {
+
+        private readonly string _testFilePath = @"C:\Users\marii\OneDrive\Desktop\BCTSO-20-NC-2-main\BCTSO-20-NC-2\MiniBank.Tests\Data\Accounts.json";
+        [Fact]
+        public void get_All_Acounts()
+        {
+            var dataTest = new AccountJsonRepository(_testFilePath);
+            
+            // Act
+            var result = dataTest.GetAccounts();
+
+            // Assert
+            Assert.Equal(6,result.Count);
+        }
+
+        [Fact]
+        public void Get_Accounts_OfCustomer()
+        {
+            var dataTest = new AccountJsonRepository(_testFilePath);
+            var customerId = 1;
+            var result = dataTest.GetAccountsOfCustomer(customerId);
+
+            Assert.Equal(3, result.Count);
+
+        }
+
+        [Fact]
+        public void Add_new_Account()
+        {
+            var dataTest = new AccountJsonRepository(_testFilePath);
+            var newAccount = new Account()
+            {
+                Id = 7,
+                Iban = "GE94SB5621487456325158",
+                Currency = "USD",
+                Balance = 23,
+                CustomerId = 1,
+                Name = "testAcount"
+            };
+            dataTest.Create(newAccount);
+            var actual= dataTest.GetAccounts().Count;
+            Assert.Equal(7, actual);
+
+        }
+        [Fact]
+        public void DeleteId()
+        {
+            var dataTest = new AccountJsonRepository(_testFilePath);
+            var  accountIdToDelete = 3;
+            var expected = 6;
+
+            dataTest.Delete(accountIdToDelete);
+
+            var actual= dataTest.GetAccounts().Count;
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
